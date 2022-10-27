bbff2e489102df75bef5cd953103c9e41c6252e6
myeongju-kim <mjoo1106@naver.com>
2022-10-27 14:59:23 +0900

    fix: delete previouse exception handling

--- src/main/java/com/buddy/study/account/exception/EmailDuplication.java
+++
@@ -1,7 +0,0 @@
-package com.buddy.study.account.exception;
-
-public class EmailDuplication extends RuntimeException{
-    public EmailDuplication(String message){
-        super(message);
-    }
-}
--- src/main/java/com/buddy/study/account/exception/LoginException.java
+++
@@ -1,7 +0,0 @@
-package com.buddy.study.account.exception;
-
-public class LoginException extends RuntimeException{
-    public LoginException(String message){
-        super(message);
-    }
-}
