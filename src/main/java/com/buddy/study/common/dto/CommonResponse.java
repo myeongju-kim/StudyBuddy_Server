b56e14be85338d60e09ed9e5d9fd9834f44f5e34
myeongju-kim <mjoo1106@naver.com>
2022-10-27 15:00:33 +0900

    feat: add constructor and response of

--- src/main/java/com/buddy/study/common/dto/CommonResponse.java
+++ src/main/java/com/buddy/study/common/dto/CommonResponse.java
@@ -7,4 +7,20 @@ public class CommonResponse {
     private String code;
     private String message;
     private Object result;
+    public CommonResponse() {
+        this.code = "0000";
+    }
+    public CommonResponse(ErrorCode code, Object result) {
+        this.message = code.getMessage();
+        this.code = code.getCode();
+        this.result = result;
+    }
+    public CommonResponse success(String message, Object result){
+        this.message=message;
+        this.result=result;
+        return this;
+    }
+    public static CommonResponse of(ErrorCode code, Object result) {
+        return new CommonResponse(code, result);
+    }
 }
--- src/main/java/com/buddy/study/common/service/CommonService.java
+++ src/main/java/com/buddy/study/common/service/CommonService.java
@@ -1,14 +1,9 @@
 package com.buddy.study.common.service;
 
-import com.buddy.study.account.domain.Account;
-import com.buddy.study.account.dto.JoinRequest;
-import com.buddy.study.account.dto.LoginRequest;
-import com.buddy.study.account.dto.LoginResponse;
-import com.buddy.study.account.exception.EmailDuplication;
-import com.buddy.study.account.exception.LoginException;
 import com.buddy.study.account.repository.AccountRepository;
 import com.buddy.study.common.dto.CommonResponse;
-import com.buddy.study.common.exception.NameDuplication;
+import com.buddy.study.common.dto.ErrorCode;
+import com.buddy.study.common.exception.ConflictException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,18 +11,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommonService {
     final private AccountRepository accountRepository;
-    public CommonResponse response(String code, String message, Object object){
-        CommonResponse commonResponse=new CommonResponse();
-        commonResponse.setCode("0000");
-        commonResponse.setMessage(message);
-        commonResponse.setResult(object);
-        return commonResponse;
-    }
+    private CommonResponse commonResponse=new CommonResponse();
     public CommonResponse checkUser(String name){
         if(accountRepository.findByName(name)!=null){
-            throw new NameDuplication("닉네임이 중복되었습니다.");
+            throw new ConflictException(ErrorCode.DUPLICATE_NAME);
         }
-        return response("0000","사용 가능한 닉네임입니다.",null);
+        return commonResponse.success("사용 가능한 이름입니다.",null);
     }
 
 }
